0bd7bc983b5acf69d1cedb33f78a3c4f0b96ed72
SOURAV-CARWALE <sourav@carwale.com>
2023-11-16 23:41:54 +0530

    position changed for skill section

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <Experience />
       <Skills />
+      <Experience />
       <Projects />
       <CodingProfile />
       <Contact />
